3fcb931d1b297dfa8e8b9c19138c7513f282f62f
Allan Weisser Verdessi <vredes@live.com>
2019-06-21 18:54:00 -0400

    v 0.6

--- mail.php
+++ mail.php
@@ -1,31 +1,28 @@
 <?php
-    $to = 'vredes@live.com';
-    $name = $_POST["name"];
-    $email= $_POST["email"];
-    $text= $_POST["message"];
-    $subject= $_POST["subject"];
-    
 
+ $to = 'vredes@live.com'; // replace this mail with yours
+ $firstname = $_POST["name"];
+ $email= $_POST["email"];
+ $text= $_POST["message"];
+ $subject= $_POST["subject"];
 
-    $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-    $message ='<table style="width:100%">
-        <tr>
-            <td>'.$name.'  '.$subject.'</td>
-        </tr>
-        <tr><td>Email: '.$email.'</td></tr>
-        <tr><td>phone: '.$subject.'</td></tr>
-        <tr><td>Text: '.$text.'</td></tr>
-        
-    </table>';
-
-    if (@mail($to, $email, $message, $headers))
-    {
-        echo '<script>alert("Enviado.")</script> ';
-    }else{
-        echo '<script>alert("No posees el puntaje requerido para esta sección.")</script> ';
-    }
+ $headers = 'MIME-Version: 1.0' . "\r\n";
+ $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+ $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+ $message ='<table style="width:100%">
+     <tbody><tr>
+         <td>'.$firstname.'  '.$subject.'</td>
+          </tr>
+          <tr><td>Email: '.$email.'</td></tr>
+          <tr><td>phone: '.$subject.'</td></tr>
+          <tr><td>Text: '.$text.'</td></tr>
+         </tbody></table>';
+        if (@mail($to, $email, $message, $headers))
+            {
+              echo 'The message has been sent.';
+            }else{
+              echo 'failed';
+            }
 
 ?>
