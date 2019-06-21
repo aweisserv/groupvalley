ae60f674649ca7bcf3c03d65c4c60042ab81c647
Allan Weisser Verdessi <vredes@live.com>
2019-06-21 19:03:43 -0400

    v 0.6

--- mail.php
+++ mail.php
@@ -2,19 +2,19 @@
 
  $to = 'vredes@live.com'; // replace this mail with yours
  $firstname = $_POST["name"];
- $email= $_POST["email"];
+ $e_mail= $_POST["email"];
  $text= $_POST["message"];
  $subject= $_POST["subject"];
 
  $headers = 'MIME-Version: 1.0' . "\r\n";
- $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
+ $headers .= "From: " . $e_mail . "\r\n"; // Sender's E-mail
  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
  
  $message ='<table style="width:100%">
      <tbody><tr>
          <td>'.$firstname.'  '.$subject.'</td>
           </tr>
-          <tr><td>Email: '.$email.'</td></tr>
+          <tr><td>Email: '.$e_mail.'</td></tr>
           <tr><td>phone: '.$subject.'</td></tr>
           <tr><td>Text: '.$text.'</td></tr>
          </tbody></table>';
