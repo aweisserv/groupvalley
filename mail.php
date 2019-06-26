bfe2eb66c7a59f2f1b00059fd743c1747f804ab5
Allan Weisser Verdessi <vredes@live.com>
2019-06-26 12:49:44 -0400

    v0.6.1

--- mail.php
+++ mail.php
@@ -2,23 +2,23 @@
 
  $to = 'vredes@live.com'; // replace this mail with yours
  $firstname = $_POST["name"];
- $e_mail= $_POST["email"];
+ $email= $_POST["email"];
  $text= $_POST["message"];
  $subject= $_POST["subject"];
 
  $headers = 'MIME-Version: 1.0' . "\r\n";
- $headers .= "From: " . $e_mail . "\r\n"; // Sender's E-mail
+ $headers .= "From: " . $email . "\r\n"; // Sender's E-mail
  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
  
  $message ='<table style="width:100%">
      <tbody><tr>
          <td>'.$firstname.'  '.$subject.'</td>
           </tr>
-          <tr><td>Email: '.$e_mail.'</td></tr>
+          <tr><td>Email: '.$email.'</td></tr>
           <tr><td>phone: '.$subject.'</td></tr>
           <tr><td>Text: '.$text.'</td></tr>
          </tbody></table>';
-        if (@mail($to, $email, $message, $headers))
+        if (mail($to, $email, $message, $headers))
             {
               echo 'The message has been sent.';
             }else{
